05cf939316812b10a6017669734f1c4bc5441f3a
marcusmellis89 <m.m.ellis89@gmail.com>
2016-05-04 22:10:38 -0700

    [setup]bookshelf,middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 var express = require('express');
-
 var app = express();
 var bodyParser = require('body-parser');
-
+var Promise = require('bluebird');
 
 var port = process.env.port || 8000;
 
+var dbConfig = {
+	client: 'mysql',
+	connection: {
+		host: 'localhost',
+		user: 'root',
+		port: port,
+		database: 'fight_historian',
+		charset: 'utf8'
+	}
+}
+
+var knex = require('knex')(dbConfig);
+var bookshelf = require('bookshelf')(knex);
+
+var allowCrossDomain = function(req, res, next){
+	res.header('Access-Control-Allow-Origin','*');
+	next();
+}
+
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 
@@ -15,5 +34,6 @@ app.use('/',function(req,res){
 	res.send('hello world')
 });
 
-app.listen(port);
-console.log('listening on port:',port);
+app.listen(port,function(){
+	console.log('listening on port:',port);
+});
